852da239be578e6b6f4da5581fcb7f8070e06b30
Aman Nindra <amannindra@gmail.com>
2026-03-20 16:44:21 -0700

    sdas

--- backend.sh
+++ backend.sh
@@ -3,7 +3,6 @@
 echo "Deleting docker images"
 sudo docker system prune -a
 
-
 echo "Stopping anything running on port 8000..."
 
 PID=$(lsof -t -i:8000)
@@ -18,7 +17,6 @@ cd backend
 echo "Checking current folder"
 pwd
 
-
 echo "Activating virtual environment"
 
 if [ -d "venv" ]; then
@@ -30,11 +28,15 @@ else
   exit 1
 fi
 
-# backend/.venv/bin/activate
+echo "Updating Pip"
+pip3 install --upgrade pip
+
+echo "Installing dependencies"
+pip3 install -r requirements.txt
+
+
 
 echo "Starting server"
 python3 main.py
 
 echo "Server started"
-
-# chmod +x backend.sh && ./backend.sh
--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ load_dotenv()
 if __name__ == "__main__":
     uvicorn.run("app.api:app", host="0.0.0.0", port=int(os.getenv("PORT")), reload=True, reload_dirs=["app"])
     
-
+    
+#  100.119.189.80
 # run the server with the command: python main.py
 # kill -9 $(lsof -t -i:8000)
